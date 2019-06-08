c33737c9f98d466236497e178d8920f5269f5ef7
adrianovalente <dri9595@gmail.com>
2019-06-07 22:19:47 -0300

    Add pi module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+const { onButtonPress, setLightState }  = require('./infra/pi')
+
 const { initConfig, getConfig } = require('./infra/config')
 const { UpdatesSubscriber } = require('./infra/updates-subscriber')
+const { LocalPersister } = require('./infra/local-persister')
+const { RemotePersister } = require('./infra/remote-persister')
 
-let updatesSubscriber
+let updatesSubscriber, localPersister, remotePersister
 
 ;(async function main () {
   const config = await initConfig().then(getConfig)
@@ -13,5 +17,13 @@ let updatesSubscriber
     onUpdate: () => {
       console.log(new Date() + ' - Just got an update! 💡')
     }
+  })
+
+  localPersister = new LocalPersister({
+    LOCAL_PERSISTER_PATH: '/home/METAL_PERSISTANCE'
+  })
+
+  remotePersister = new RemotePersister({
+    API_URL: config.API_URL
   })
 })()
---
+++ src/infra/pi.js
@@ -0,0 +1,41 @@
+const isPi = require('detect-rpi')
+let Gpio
+
+if (isPi()) {
+  Gpio = require('onoff').Gpio
+  console.log('Running on pi environment')
+} else {
+  Gpio = class GpioMock {
+    constructor (pin) {
+      this.pin = pin
+      console.log(`Mocked pi.`)
+    }
+
+    watch () {
+      console.log(`Watching pin ${this.pin}`)
+    }
+
+    writeSync(value) {
+      console.log(`Writing ${value} on pin ${this.pin}`)
+    }
+  }
+}
+
+const output = new Gpio(2, 'out')
+const input = new Gpio(3, 'in', 'both', {
+  debounceTimeout: 10
+})
+
+module.exports.onButtonPress = function onButtonPress (cb) {
+  input.watch((err, value) => {
+    if (err) throw err
+
+    if (value === 0) {
+      cb()
+    }
+  })
+}
+
+module.exports.setLightState = function setLightState (state) {
+  return output.writeSync(state ? 1 : 0)
+}
