3d0d233fad679f861a0aa17f3b9ba63d2bd911a8
adrianovalente <dri9595@gmail.com>
2019-10-10 21:04:43 -0300

    Fix deploy ~sera?~

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+pip install --user awscli
 
-sshpass -p $SSH_PASS ssh -p $SSH_PORT $SSH_HOST /home/pi/deployment/deployment-agent/update-metal.sh
+KEY_FILE = $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
+aws s3 cp s3://pizza-hut-coupon/raspberrier /tmp/$KEY_FILE
+
+ssh -p $SSH_PORT $SSH_HOST -i /tmp/$KEY_FILE /home/pi/deployment/deployment-agent/update-metal.sh
+
+rm /tmp/$KEY_FILE
